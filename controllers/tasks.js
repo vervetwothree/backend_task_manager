1b7e4dccb6757eb1c48b48d16a1f9511cd54660b
vervetwothree <jeepon35@gmail.com>
2022-10-17 17:48:55 -0400

    hello, dad

---
+++ controllers/tasks.js
@@ -0,0 +1,70 @@
+
+//const express = require('express')
+const Task = require('../models/Task')
+
+//res.send('All Items Improved (get all tasks)')
+const getAllTasks = async (req,res) =>{
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({ tasks })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+}
+//create task 
+const createTask = async (req,res) => {
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+}
+//get single task 
+const getTask = async (req,res) => {
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOne({_id:taskID});
+    if(!task){
+        return res.status(404).json({msg: `No task with id: ${taskID}`})
+    }
+        
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({ msg: error }) 
+    }
+}
+//update task 
+const updateTask = async (req, res) => {
+    try {
+        const {id:taskID} = req.params;
+        const task = await Task.findOneAndUpdate({_id: taskID}, req.body, { new:true, runValidators: true})
+    if(!task){
+        return res.status(404).json({msg: `No task with id: ${taskID}`})
+    }
+    res.status(200).json({task})
+    
+    } catch (error) {
+        res.status(500).json({ msg: error }) 
+    }
+    
+}
+//delete dask 
+const deleteTask = async (req,res) => {
+    try {
+        const {id: taskID} = req.params
+        const task = await Task.findOneAndDelete({_id:taskID});
+    if (!task) {
+        return res.status(404).json({msg: `No task with id: ${taskID}`})
+        }
+        res.status(200).json({ task })
+        
+        } catch (error) {
+            res.status(500).json({ msg: error })
+    }
+}
+
+module.exports = {
+    getAllTasks, getTask, createTask, updateTask, deleteTask
+}
+
